9cd73250d6e68130311cbb55aa939e274f806543
Leo <leonelsoto17@gmail.com>
2022-01-14 13:24:56 -0600

    checkwinner function in the game module

--- script.js
+++ script.js
@@ -21,25 +21,37 @@ const displayController = (() => {
 const Player = (name, playerMove) => {
     return {name, playerMove}
 }
-let jeff = Player('jeff', 'X')
-let kim = Player('Kim', 'O')
+
 const game = (() => {
     let domBoard = document.querySelectorAll('.container .grid-cell')
     let turn;
     const _updateTurn = (playerOne, playerTwo) => {
         if(!turn) {
             turn = playerOne.playerMove;
-            console.log('he')
             return
         }
         turn = (turn === playerOne.playerMove) ? playerTwo.playerMove : playerOne.playerMove
     }
-    let _clickHandler = (index) => {
+    const _clickHandler = (index) => {
         gameBoard.updateBoard(index, turn)
 
         displayController.render()
     }
-    let init = (playerOne, playerTwo) => {
+    const checkWinner = () => {
+        let board = gameBoard.getBoard()
+        
+        const allEqual = arr => arr.every(val => val === arr[0]);
+        let winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
+                         [0,3,6], [1,4,7], [2,5,8], [6,4,2]]
+        let win = winCombos.some(combo => {
+            let boardCombo = [board[combo[0]], board[combo[1]], board[combo[2]]]
+            return allEqual(boardCombo) && board[combo[0]]
+        }) 
+        if(win) {
+            console.log('Winner')
+        } else console.log('no Winner')
+    }
+    const init = (playerOne, playerTwo) => {
         _updateTurn(playerOne, playerTwo)
         domBoard.forEach((cell, index) => cell.addEventListener('click', () => {
             if(cell.textContent) {
@@ -47,7 +59,11 @@ const game = (() => {
             }
             _clickHandler(index)
             _updateTurn(playerOne, playerTwo)
+            checkWinner()
         }))            
     }
-    return {init}
+    return {init, checkWinner}
 })()
+
+let jeff = Player('jeff', 'X')
+let kim = Player('Kim', 'O')
