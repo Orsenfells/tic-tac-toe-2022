1a3ccedb99dda2b5697c8e0ac9ad3a8ffee93b13
Leo <leonelsoto17@gmail.com>
2022-01-14 12:33:52 -0600

    logic for clicking on cells

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const gameBoard = (() => {
                  '','','']
 
     const getBoard = () => board
-    const updateBoard = (index, move) => board[index] = move 
+    const updateBoard = (index, move) => {
+        board[index] = move
+    } 
     return {getBoard, updateBoard}                 
 })()
 const displayController = (() => {
@@ -23,11 +25,28 @@ let jeff = Player('jeff', 'X')
 let kim = Player('Kim', 'O')
 const game = (() => {
     let domBoard = document.querySelectorAll('.container .grid-cell')
-    let init = (playerOne, PlayerTwo) => {
+    let turn;
+    const _updateTurn = (playerOne, playerTwo) => {
+        if(!turn) {
+            turn = playerOne.playerMove;
+            console.log('he')
+            return
+        }
+        turn = (turn === playerOne.playerMove) ? playerTwo.playerMove : playerOne.playerMove
+    }
+    let _clickHandler = (index) => {
+        gameBoard.updateBoard(index, turn)
+
+        displayController.render()
+    }
+    let init = (playerOne, playerTwo) => {
+        _updateTurn(playerOne, playerTwo)
         domBoard.forEach((cell, index) => cell.addEventListener('click', () => {
-            gameBoard.updateBoard(index, playerOne.playerMove)
-            console.log(gameBoard.getBoard())
-            displayController.render()
+            if(cell.textContent) {
+                return
+            }
+            _clickHandler(index)
+            _updateTurn(playerOne, playerTwo)
         }))            
     }
     return {init}
